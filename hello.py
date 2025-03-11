22e357fcbefc730dcc2d8f605b93d9202360a248
M. Jabs <m.jabs@bbk.schule>
2025-03-11 13:55:20 +0100

    Beschreibenden Text verbessert

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ def main():
     # Hier wird Hello World! ausgegeben; aber es soll diesmal mit einem Zeilenumbruch nach Hello ausgegeben werden:
     # Hello 
     # World! 
-    # Achtung: In der neuen Zeile darf kein Leerzeichen sein!
     print("Hello World!")   
 
 if __name__ == "__main__":
