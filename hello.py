075608b8e36afd6ad2de7248b090187f217f28de
M. Jabs <80675651+BBkJabs@users.noreply.github.com>
2025-03-11 08:38:58 +0100

    Update hello.py
    Kommentar angepasst

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 def main():
-    # Hier wird Hello! ausgegeben; aber es soll Hello World! ausgegeben werden
+    # Hier wird Hello World! ausgegeben; aber es soll diesmla mit einem Zeilenumbruch nach hello ausgegeben werden:
+    # Hello 
+    # World! 
     print("Hello World!")   
 
 if __name__ == "__main__":
